205a08206270ec8a8ca8a2fb3dedaaabb8e38c4d
Desmond Yeow <desmond.yeow@Desmonds-MacBook-Pro.local>
2022-05-21 20:05:08 +0800

    Added sha256 hash algo.

--- generator/generator.go
+++ generator/generator.go
@@ -1,12 +1,14 @@
 package generator
 
 import (
+	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
 )
 
 type Generator interface {
-	GenerateTrackingId(stringInput string) (trackingId string)
+	GenerateSHA512TrackingId(stringInput string) (trackingId string)
+	GenerateSHA256TrackingId(stringInput string) (trackingId string)
 }
 
 type generator struct {
@@ -19,10 +21,19 @@ func NewGenerator(masterKey string, length int) Generator {
 	return &generator{masterKey: masterKey, length: length}
 }
 
-// GenerateTrackingId is used to generate a N-length tracking ID by accepting a string input.
-func (generator *generator) GenerateTrackingId(stringInput string) (trackingId string) {
+// GenerateSHA512TrackingId is used to generate a N-length tracking ID using SHA-512 hash algo by accepting a string input.
+func (generator *generator) GenerateSHA512TrackingId(stringInput string) (trackingId string) {
 	data := generator.masterKey + stringInput
 	hash := sha512.New()
+	hash.Write([]byte(data))
+	byteString := hash.Sum(nil)
+	return fmt.Sprintf("%x", byteString)[:generator.length]
+}
+
+// GenerateSHA256TrackingId is used to generate a N-length tracking ID using SHA-256 hash algo by accepting a string input.
+func (generator *generator) GenerateSHA256TrackingId(stringInput string) (trackingId string) {
+	data := generator.masterKey + stringInput
+	hash := sha256.New()
 	hash.Write([]byte(data))
 	byteString := hash.Sum(nil)
 	return fmt.Sprintf("%x", byteString)[:generator.length]
